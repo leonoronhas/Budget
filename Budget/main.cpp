739690eadeb6fd0124c5b11580c21635ae578cf6
leonoronsantos <leosud099@gmail.com>
2019-10-19 22:42:45 -0600

    Add project files.

---
+++ Budget/main.cpp
@@ -0,0 +1,27 @@
+/*******************************************************************************
+*  Budget wise
+*  Author: Leonardo Santos
+*  Version 1.0
+*  Updated: 10/19/2019
+*
+*  Budget wise will calculate any monthly income with taxes based in Utah/Idaho
+*  After a set of questions, a monthly budget will be displayed.
+******************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+void promptNumbers() {
+
+}
+
+int main()
+{
+	std::vector<int> monthlyAmounts;
+
+
+	return 0;
+
+}
