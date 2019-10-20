daa0fa630d3ed63b494cd7266bd065236f55cec2
leonoronsantos <leosud099@gmail.com>
2019-10-19 23:13:17 -0600

    menu display started

--- Budget/main.cpp
+++ Budget/main.cpp
@@ -13,7 +13,15 @@
 #include <vector>
 
 
-void promptNumbers() {
+void menuDisplay() {
+
+	std::cout << "BUDGET WISE\n";
+	std::cout << "Budget solution by Leo Santos\n" << std::endl;
+	std::cout << "Choose what kind of budget you want to work on:\n" << std::endl;
+	std::cout << "  Monthly press - 1\n";
+	std::cout << "  Yearly  press - 2\n";
+	std::cout << "  Quit    press - 0\n";
+
 
 }
 
@@ -21,7 +29,12 @@ int main()
 {
 	std::vector<int> monthlyAmounts;
 
+	menuDisplay();
 
+
+
+	int ind;
+	std::cin >> ind;
 	return 0;
 
 }
