c1d1d15885716210d6f68bad700d2cc492f5d6f0
leonoronsantos <leosud099@gmail.com>
2019-10-27 18:38:49 -0600

    Added amount class, updated main

--- Budget/main.cpp
+++ Budget/main.cpp
@@ -2,7 +2,7 @@
 *  Budget wise
 *  Author: Leonardo Santos
 *  Version 1.0
-*  Updated: 10/19/2019
+*  Updated: 10/27/2019
 *
 *  Budget wise will calculate any monthly income with taxes based in Utah/Idaho
 *  After a set of questions, a monthly budget will be displayed.
@@ -13,23 +13,26 @@
 #include <vector>
 
 
-void menuDisplay() {
+void menuDisplay(int choice) {
 
-	std::cout << "BUDGET WISE\n";
-	std::cout << "Budget solution by Leo Santos\n" << std::endl;
-	std::cout << "Choose what kind of budget you want to work on:\n" << std::endl;
-	std::cout << "  Monthly press - 1\n";
-	std::cout << "  Yearly  press - 2\n";
-	std::cout << "  Quit    press - 0\n";
+	do {
+		std::cout << "BUDGET WISE\n";
+		std::cout << "Budget solution by Leo Santos\n" << std::endl;
+		std::cout << "Choose what kind of budget you want to work on:\n" << std::endl;
+		std::cout << "  Monthly press - 1\n";
+		std::cout << "  Yearly  press - 2\n";
+		std::cout << "  Quit    press - 0\n";
+	} while (choice != 0);
 
 
 }
 
 int main()
 {
-	std::vector<int> monthlyAmounts;
+	int choice = -1;
 
-	menuDisplay();
+
+	menuDisplay(choice);
 
 
 
